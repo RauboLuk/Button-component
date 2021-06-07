69c35acb796de498286bcebd577dfba2960fde9c
RauboLuk <rauboluk@gmail.com>
2021-06-07 22:57:48 +0200

    feat: text button, more readable code for variants

--- src/Button.js
+++ src/Button.js
@@ -2,21 +2,44 @@ import "./Button.css";
 import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles({
-  button: {
-    background: (props) => props.outline ? "none" : "#E0E0E0",
-    border: (props) => props.outline ? "1px solid #3D5AFE" : 0,
+  button: (props) => ({
+    outline: 0,
+    background: "#E0E0E0",
+    border: 0,
     borderRadius: 6,
-    boxShadow: (props) => props.outline ? "none" : "0px 2px 3px rgba(51, 51, 51, 0.2)",
-    color: (props) => props.outline ? "#3D5AFE" : "#3F3F3F",
+    boxShadow: "0px 2px 3px rgba(51, 51, 51, 0.2)",
+    color: "#3F3F3F",
     fontFamily: "Noto Sans JP",
     fontWeight: 500,
     fontSize: 14,
     height: 36,
     padding: "0 16px",
-    "&:hover, &:focus": {
-      background: (props) => props.outline ? "rgba(41, 98, 255, 0.1)" : "#AEAEAE",
-    },
-  },
+    ...(props.variant === "outline" && {
+      background: "none",
+      border: "1px solid #3D5AFE",
+      boxShadow: "none",
+      color: "#3D5AFE",
+    }),
+    ...(props.variant === "text" && {
+      background: "none",
+      border: 0,
+      boxShadow: "none",
+      color: "#3D5AFE",
+    }),
+    ...(props.disableShadow && {
+      boxShadow: "none",
+    }),
+    
+    "&:hover, &:focus": (props) => ({
+      background: "#AEAEAE",
+      ...(props.variant === "outline" && {
+        background: "rgba(41, 98, 255, 0.1)",
+      }),
+      ...(props.variant === "text" && {
+        background: "rgba(41, 98, 255, 0.1)",
+      }),
+    }),
+  }),
 });
 
 const Button = ({ placeholder = "Default", ...props }) => {
