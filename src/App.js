a9c464cec5d10c04c1ae1524d821453e1a26244f
RauboLuk <rauboluk@gmail.com>
2021-06-13 23:49:46 +0200

    feat: page layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Button from "./Button";
 
@@ -6,23 +5,82 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button />
-        <Button variant="outline" />
-        <Button variant="text" />
-        <Button disableShadow />
-        <Button disabled />
-        <Button variant="text" disabled />
-        <Button startIcon="access_alarm" />
-        <Button endIcon="local_grocery_store" />
-        <Button size="sm" />
-        <Button size="md" />
-        <Button size="lg" />
-        <Button color="default" />
-        <Button color="primary" />
-        <Button color="secondary" />
-        <Button color="danger" />
+        <h1>Buttons</h1>
       </header>
+      <main className="Main">
+        <section>
+          <p>{`<Button />`}</p>
+          <Button />
+        </section>
+        <section>
+          <p>{`<Button variant=”outline” />`}</p>
+          <Button variant="outline" />
+        </section>
+        <section>
+          <p>{`<Button variant=”text” />`}</p>
+          <Button variant="text" />
+        </section>
+        <section>
+          <p>{`<Button disableShadow />`}</p>
+          <Button disableShadow />
+        </section>
+        <section>
+          <p>{`<Button disabled />`}</p>
+          <Button disabled />
+        </section>
+        <section>
+          <p>{`<Button variant="text" disabled />`}</p>
+          <Button variant="text" disabled />
+        </section>
+        <section>
+          <p>{`<Button startIcon="access_alarm" />`}</p>
+          <Button startIcon="access_alarm" />
+        </section>
+        <section>
+          <p>{`<Button endIcon="local_grocery_store" />`}</p>
+          <Button endIcon="local_grocery_store" />
+        </section>
+        <section>
+          <p>{`<Button size="sm" />`}</p>
+          <Button size="sm" />
+        </section>
+        <section>
+          <p>{`<Button size="md" />`}</p>
+          <Button size="md" />
+        </section>
+        <section>
+          <p>{`<Button size="lg" />`}</p>
+          <Button size="lg" />
+        </section>
+        <section>
+          <p>{`<Button color="default" />`}</p>
+          <Button color="default" />
+        </section>
+        <section>
+          <p>{`<Button color="primary" />`}</p>
+          <Button color="primary" />
+        </section>
+        <section>
+          <p>{`<Button color="secondary" />`}</p>
+          <Button color="secondary" />
+        </section>
+        <section>
+          <p>{`<Button color="danger" />`}</p>
+          <Button color="danger" />
+        </section>
+        <section>
+          <p>{`<Button color="secondary" variant=”outline” />`}</p>
+          <Button color="secondary" variant="outline" />
+        </section>
+        <section>
+          <p>{`<Button color="danger" disableShadow />`}</p>
+          <Button color="danger" disableShadow />
+        </section>
+        <section>
+          <p>{`<Button color="primary" variant="text" />`}</p>
+          <Button color="primary" variant="text" />
+        </section>
+      </main>
     </div>
   );
 }
